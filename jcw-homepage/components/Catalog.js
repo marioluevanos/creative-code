0bde8042f4c3fff842623d917f7e81034d255ff8
Mario Luevanos <mario.luevanos@byteme.com>
2024-04-11 08:28:25 -0700

    JCW Update

--- jcw-homepage/components/Catalog.js
+++ jcw-homepage/components/Catalog.js
@@ -3,7 +3,34 @@ import Button from "./Button.js";
 
 const template = `
 <section id="catalog" ref="root">
-  <Button class="ghost white filter" :class="isFiltersActive ? 'active' : undefined" @click="onToggleFilterClick">
+  <header class="header">
+
+    <!-- <svg class="icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 30.9 22.7">
+      <rect fill="none" width="30.9" height="22.7"/>
+      <path class="fill-1" d="M13.9,16.1c0-0.9,0.8-1.7,1.7-1.7s1.7,0.8,1.7,1.7c0,0.9-0.8,1.7-1.7,1.7S13.9,17,13.9,16.1z"/>
+      <path class="fill-1" d="M30.9,13.9v7.7c0,0.6-0.5,1.1-1.1,1.1H1.1c-0.6,0-1.1-0.5-1.1-1.1v-7.7h13.1v2.2c0,1.4,1.1,2.5,2.5,2.5
+        c1.4,0,2.5-1.1,2.5-2.5v-2.2H30.9z"/>
+      <path class="fill-1" d="M26.3,4.4H5L0.2,9.3h30.5L26.3,4.4z"/>
+      <path class="fill-1" d="M9.7,5.4L8.5,4.7c0,0,0.3-0.6,1.5-2.9C10.9,0.1,12,0,13,0h3.9v1.4H13c-0.7,0-1.2,0-1.7,1
+        C10.1,4.9,9.7,5.4,9.7,5.4z"/>
+      <path class="fill-1" d="M21.8,5.4c0,0-0.3-0.6-1.6-3c-0.5-1-1-1-1.7-1h-3.9V0h3.9c1,0,2.1,0.1,2.9,1.8C22.7,4.2,23,4.7,23,4.7
+        L21.8,5.4L21.8,5.4z"/>
+      <path class="fill-2" d="M30.9,10H0v0.7h30.9V10z"/>
+      <path class="fill-2" d="M30.9,11.2H0v0.7h30.9V11.2z"/>
+      <path class="fill-2" d="M30.9,12.5H0v0.7h30.9V12.5z"/>
+    </svg> -->
+
+    <h2 class="fs-h3 title">Catalog Throwback</h2>
+    <p class="fs-small">
+    Step back in time with JC Whitney's vintage catalog gallery, showcasing decades of car parts & accessories that fueled automotive passions.
+    </p>
+  </header>
+  <Button
+    class="ghost filter"
+    :class="isFiltersActive ? 'active' : undefined"
+    @click="onToggleFilterClick"
+    :animated="!isFiltersActive"
+  >
     Filter Keywords
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><title>arrow-sm-down</title><g fill="currentColor" stroke-linecap="square" stroke-linejoin="miter" stroke-miterlimit="10"><polyline fill="none" stroke="currentColor" stroke-width="2" points="16,10 12,14 8,10 " transform="translate(0, 0)"></polyline></g></svg>
   </Button>
@@ -26,12 +53,11 @@ const template = `
   </div>
   <div
     class="catalogs"
-    :class="{ loops: catalogs.length >= min }"
+    :class="{ loops, static: !loops }"
     ref="catalogs"
     id="catalogs"
   >
     <figure
-      @click="onCatalogItemClick"
       class="item"
       v-for="(image, index) in catalogs"
       :key="image.src + index + image.keywords"
@@ -40,6 +66,7 @@ const template = `
     >
       <img
         class="image"
+        @click="onCatalogItemClick"
         :style="{ '--ar': image.width / image.height }"
         :src="image.src"
         :width="image.width"
@@ -56,7 +83,9 @@ const template = `
   :data-active="isCatalogView || undefined"
   @click="onCatalogItemClick"
 >
+  <article class="modal">
 
+  </article>
 </section>
 `;
 
@@ -70,13 +99,16 @@ const Catalog = {
       filters: new Set(),
       images: [],
       isCatalogView: false,
-      loop: undefined,
       isFiltersActive: false,
-      min: 3,
+      loop: undefined,
       isBusy: false,
+      min: 3,
     };
   },
   computed: {
+    loops() {
+      return this.catalogs.length >= this.min;
+    },
     keywords() {
       return Object.entries(
         this.images.reduce((keywords, item) => {
@@ -122,19 +154,37 @@ const Catalog = {
     killDraggable() {
       this.isBusy = true;
 
-      return new Promise((resolve) => {
+      const kill = () => {
+        console.log("%cKILLING...", "color: white; background: red");
         if (this.loop?.draggable) {
           this.loop.draggable.kill();
         }
 
         if (this.loop) {
-          this.loop.revert();
+          this.loop.progress(0).revert().kill();
+          Array.from(this.$refs.catalogs.children).forEach(
+            (item) => (item.style = null)
+          );
         }
+      };
 
+      return new Promise((resolve) => {
+        kill();
         setTimeout(() => {
           this.isBusy = false;
           resolve(true);
-        }, 0);
+
+          console.log("%cKILLED DRAGGABLE", "color: red");
+          if (this.catalogs.length >= this.min) {
+            console.log("%cDRAGGABLE", "color: green");
+            this.initDraggable(this.$refs.catalogs);
+          } else {
+            console.log("%cNO DRAG EXISTS", "color: gray");
+            Array.from(this.$refs.catalogs?.children).forEach((item) =>
+              item.classList.add("active")
+            );
+          }
+        }, 100);
       });
     },
     onKeywordClick(event) {
@@ -147,19 +197,7 @@ const Catalog = {
         this.filters.add(keyword);
       }
 
-      console.log("KILLING DRAGGABLE...", this.loop);
-
-      // this.killDraggable().then(() => {
-      //   console.log("%cKILLED DRAGGABLE", "color: red", this.loop);
-      //   if (this.catalogs.length >= this.min) {
-      //     console.log("%cDRAGGABLE", "color: green", this.loop);
-      //     this.initDraggable(this.$refs.catalogs);
-      //   } else {
-      //     Array.from(this.$refs.catalogs?.children).forEach((item) =>
-      //       item.classList.add("active")
-      //     );
-      //   }
-      // });
+      this.killDraggable();
     },
     onToggleFilterClick(event) {
       event.preventDefault();
@@ -187,9 +225,13 @@ const Catalog = {
         center: true,
         snap: true,
         onChange: (target) => {
-          Array.from(target.parentElement.children).forEach((el) => {
+          const parent = target.parentElement;
+          if (!parent) return;
+
+          Array.from(parent.children).forEach((el) => {
             el.classList.remove("active");
           });
+
           target.classList.add("active");
         },
       });
